81a6b4e24e9b87807407d942ad13ad00cd857ec9
gucio321 <gucio321@protonmail.com>
2024-11-12 17:16:24 +0100

    initial commit

---
+++ module1/vendor/vendor.go
@@ -0,0 +1,7 @@
+package vendor
+
+import "fmt"
+
+func Fn() {
+	fmt.Println("Hello world from vendor package")
+}
---
+++ module2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gucio321/vendor-package-issue/vendor"
+)
+
+func main() {
+	fmt.Println("vim-go")
+	vendor.Fn()
+}
