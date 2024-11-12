440ea15d33337bdabcaa3bbcfd7973bfe3e7e524
gucio321 <gucio321@protonmail.com>
2024-11-12 18:30:16 +0100

    try something else

--- module1/m1.go
+++ module1/m1.go
@@ -1,6 +1,6 @@
 package module1
 
-import "github.com/gucio321/vendor-package-issue/module1/vendor"
+import "github.com/gucio321/vendor-package-issue/module1/pkg/vendor"
 
 func Foo() {
 	vendor.Foo()
